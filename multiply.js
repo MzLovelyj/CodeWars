0d953dc2d583faf3c938fd8de37907f53fcfe6dc
mzlovelyj <liljp064@gmail.com>
2018-03-01 14:25:10 -0500

    Multiply

---
+++ multiply.js
@@ -0,0 +1,14 @@
+/*The code does not execute properly. 
+Try to figure out why.
+
+function multiply(a, b){
+  a * b
+}
+
+*/
+
+var product = 8 * 10;
+
+function multiply(a, b) {
+    return a * b;
+}
