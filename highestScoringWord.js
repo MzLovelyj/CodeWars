0080522aef6784f5fafe67cda9e00b2554c771cd
mzlovelyj <liljp064@gmail.com>
2018-10-09 23:49:39 -0400

    Another Excerise

---
+++ highestScoringWord.js
@@ -0,0 +1,27 @@
+/** Given a string of words, you need to find the highest scoring word.
+
+Each letter of a word scores points according to it's position in the alphabet: a = 1, b = 2, c = 3 etc.
+
+You need to return the highest scoring word as a string.
+
+If two words score the same, return the word that appears earliest in the original string.
+
+All letters will be lowercase and all inputs will be valid. */
+
+function high(x) {
+  //transform the input string into array & define a string of alphabetical latin characters
+  var arr = x.split(' ');
+  var str = 'abcdefghijklmnopqrstuvwxyz';
+  //Iterate through the array with input words to find the one with the greatest sum
+  var newArr = arr.map(function (word) {
+    var sum = 0;
+    for (var i = 0; i < word.length; i++) {
+      sum += str.indexOf(word[i]);
+    }
+    return sum;
+  });
+  //Return the word with the greatest sum
+  return arr[newArr.indexOf(Math.max(...newArr))];
+}
+
+console.log(high('lets go to colombia and site see the good places')); //colombia
